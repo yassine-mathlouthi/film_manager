8e7750ff3eef252e39dbe965fc93fb2dc0e12557
yassine mathlouthi <yassinemt08@gmail.com>
2025-11-19 22:46:45 +0100

    adding fire base options

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
+import 'firebase_options.dart';
 import 'core/theme/app_theme.dart';
 import 'core/providers/auth_provider.dart';
 import 'core/providers/users_provider.dart';
 import 'core/router/app_router.dart';
 
-void main() {
-  runApp(const FilmManagerApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(FilmManagerApp());
 }
 
 class FilmManagerApp extends StatelessWidget {
